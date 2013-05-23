10a0143e0154dbb747ea6d6a5ad3446ef43a8612
Nicolas Denisi <nicolas.denisi@hotmail.fr>
2013-05-23 19:07:31 +0200

    0.1 (Build #1)

---
+++ src/fr/vaevictis/vvcdb/VVCdB.java
@@ -0,0 +1,19 @@
+package fr.vaevictis.vvcdb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VVCdB extends JavaPlugin
+{
+	private VVCdBListener listener;
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
---
+++ src/fr/vaevictis/vvcdb/VVCdBListener.java
@@ -0,0 +1,13 @@
+package fr.vaevictis.vvcdb;
+
+import org.bukkit.event.Listener;
+
+public class VVCdBListener implements Listener 
+{
+	private VVCdB plugin;
+
+	public VVCdBListener(VVCdB plugin)
+	{
+		this.plugin = plugin;
+	}
+}
